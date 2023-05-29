c0b269d500a367d065da27208792854d8865dcda
Matheus Ribeiro <matheus.junio159@gmail.com>
2023-05-29 08:35:17 -0300

    Cardapio integrado com o admin.

--- src/main/java/br/com/valhalla/ohwaiterapi/controller/CardapioController.java
+++ src/main/java/br/com/valhalla/ohwaiterapi/controller/CardapioController.java
@@ -1,13 +1,19 @@
 package br.com.valhalla.ohwaiterapi.controller;
 
 import br.com.valhalla.ohwaiterapi.dto.CardapioDTO;
+import br.com.valhalla.ohwaiterapi.dto.CategoriaDTO;
+import br.com.valhalla.ohwaiterapi.exception.JaExisteException;
 import br.com.valhalla.ohwaiterapi.service.CardapioService;
+import br.com.valhalla.ohwaiterapi.service.CategoriaService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +22,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/cardapios")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CardapioController {
 
     private final CardapioService cardapioService;
@@ -24,25 +31,45 @@ public class CardapioController {
         this.cardapioService = cardapioService;
     }
 
+    @PostMapping
+    public ResponseEntity<?> salvarCardapio(@Valid @RequestBody CardapioDTO cardapioDTO) {
+        try {
+            CardapioDTO cardapioSalvo = cardapioService.salvarOuAlterar(cardapioDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body(cardapioSalvo);
+        } catch (JaExisteException ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage()); // ou uma mensagem de erro personalizada
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro desconhecido"); // ou uma mensagem de erro personalizada
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> alterarCardapio(@PathVariable Long id, @RequestBody CardapioDTO cardapioDTO) {
+        try {
+            cardapioDTO.setId(id);
+            CardapioDTO cardapioEditado = cardapioService.salvarOuAlterar(cardapioDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body(cardapioEditado);
+        } catch (JaExisteException ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage()); // ou uma mensagem de erro personalizada
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro desconhecido"); // ou uma mensagem de erro personalizada
+        }
+    }
+
     @GetMapping
-    public ResponseEntity<List<CardapioDTO>> listarCardapios() {
+    public ResponseEntity<List<CardapioDTO>> buscarTodosCardapios() {
         List<CardapioDTO> cardapios = cardapioService.buscarTodos();
         return ResponseEntity.ok(cardapios);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<CardapioDTO> obterCardapio(@PathVariable Long id) {
-        CardapioDTO cardapio = cardapioService.obterCardapio(id);
-        if (cardapio != null) {
-            return ResponseEntity.ok(cardapio);
+        CardapioDTO cardapioDTO = cardapioService.obterCardapio(id);
+        if (cardapioDTO != null) {
+            return ResponseEntity.ok(cardapioDTO);
+        } else {
+            return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.notFound().build();
-    }
-
-    @PostMapping
-    public ResponseEntity<CardapioDTO> adicionarCardapio(@RequestBody CardapioDTO cardapioDTO) {
-        CardapioDTO novoCardapio = cardapioService.salvarOuAlterar(cardapioDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(novoCardapio);
     }
 
     @DeleteMapping("/{id}")
